41ee83009089bc91e32aadf739689319988d4846
monicalaura <monicalauraburns@yahoo.com>
2020-04-24 15:35:28 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [advice, setAdvice] = useState('CLICK AND GET A TIP!');
+
+
+ //fetchTip function
+ const fetchTip = () => {
+   axios.get("https://api.adviceslip.com/advice")
+   .then((response) => {
+     const {advice} = response.data.slip; 
+     setAdvice(advice);
+    console.log(advice)
+   })
+   .catch((error) => {
+    console.log(error)
+   });
+ }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className ="container">
+      <header>
+        <h1>MyTIPBuddy</h1>
       </header>
+ 
+      <div className ="tip" onClick={fetchTip}>
+     <h2>{advice}</h2>
+        </div>
+        
+      <div className = "robot-wrapper">
+        <button className = "robotBtn" onClick={fetchTip}><span> Get a <br/> TIP  
+        </span>
+        </button>
+      </div>
+      <footer>&copy; Copyright 2020 <a href ="https://monicalaura.github.io/portfolio" target ="_blank"><em>Monica Laura</em> </a></footer>
+
     </div>
   );
 }
